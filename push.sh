13f896d2b806d001849303d871968c6aac8476be
RatnadeepBose69420 <boseratnadeep79@gmail.com>
2025-11-29 03:20:23 +0530

    Auto update - 2025-11-29 03:20:23

--- push.sh
+++ push.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-git add .
-git commit -m "update"
+
+cd "/c/Users/boser/Downloads/falppy.fun" || exit
+
+git pull --rebase
+git add -A
+git commit -m "Auto update - $(date '+%Y-%m-%d %H:%M:%S')"
 git push
+
+echo "===================================="
+echo "   🚀 GIT SYNC COMPLETE SUCCESSFULLY"
+echo "===================================="
